2144bf18bee7d64affed46ce0c64f0feb5bc1b83
jdbs9514 <jdbs9514@gmail.com>
2022-08-30 11:36:48 -0500

    add reducer and action file

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,9 @@
-import { useDispatch } from 'react-redux/es/exports';
-import React, { useRef } from 'react';
-import uniqid from 'uniqid';
-import { bookAdd } from '../Redux/Books/Books';
-
 function Form() {
-  const dispatch = useDispatch();
-  const title = useRef(null);
-  const author = useRef(null);
-
   return (
     <form className="form">
       <input type="text" className="inputbook" placeholder="Book" required />
       <input type="text" className="inputauthor" placeholder="Author" required />
-      <button type="submit" onClick={() => dispatch(bookAdd(title.current.value, author.current.value, uniqid()))} className="submit">Submit</button>
+      <button type="submit" className="submit">Submit</button>
     </form>
   );
 }
