321394945774e62a5285a2f320a4c588739585c9
jdbs9514 <jdbs9514@gmail.com>
2022-08-29 22:25:26 -0500

    Add redux

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,18 @@
+import { useDispatch } from 'react-redux/es/exports';
+import React, { useRef } from 'react';
+import uniqid from 'uniqid';
+import { bookAdd } from '../Redux/Books/Books';
+
 function Form() {
-  <div className="formtitle">
-    <h2 className="newbook">Add new book</h2>
-  </div>;
+  const dispatch = useDispatch();
+  const title = useRef(null);
+  const author = useRef(null);
+
   return (
     <form className="form">
-      <input type="text" className="inputbook" placeholder="Book" />
-      <input type="text" className="inputauthor" placeholder="Author" />
-      <button type="submit" className="submit">Submit</button>
-
+      <input type="text" className="inputbook" placeholder="Book" required />
+      <input type="text" className="inputauthor" placeholder="Author" required />
+      <button type="submit" onClick={() => dispatch(bookAdd(title.current.value, author.current.value, uniqid()))} className="submit">Submit</button>
     </form>
   );
 }
